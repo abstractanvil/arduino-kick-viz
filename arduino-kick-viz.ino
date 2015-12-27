c4e690c5488ebd3cc394355faacebb4874ecae4f
Eric Sakmar <eric.sakmar@gmail.com>
2015-12-27 00:14:51 -0500

    changes to a simpler sound level algorithm

--- arduino-kick-viz.ino
+++ arduino-kick-viz.ino
@@ -1,49 +1,16 @@
-/* LED "Color Organ" for Adafruit Trinket and NeoPixel LEDs.
-
-Hardware requirements:
- - Adafruit Trinket or Gemma mini microcontroller (ATTiny85).
- - Adafruit Electret Microphone Amplifier (ID: 1063)
- - Several Neopixels, you can mix and match
-   o Adafruit Flora RGB Smart Pixels (ID: 1260)
-   o Adafruit NeoPixel Digital LED strip (ID: 1138)
-   o Adafruit Neopixel Ring (ID: 1463)
-
-Software requirements:
- - Adafruit NeoPixel library
-
-Connections:
- - 5 V to mic amp +
- - GND to mic amp -
- - Analog pinto microphone output (configurable below)
- - Digital pin to LED data input (configurable below)
-
-Written by Adafruit Industries.  Distributed under the BSD license.
-This paragraph must be included in any redistribution.
-*/
 #include <Adafruit_NeoPixel.h>
 
-#define N_PIXELS  30  // Number of pixels you are using
-#define MIC_PIN    1  // Microphone is attached to Trinket GPIO #2/Gemma D2 (A1)
-#define LED_PIN    0  // NeoPixel LED strand is connected to GPIO #0 / D0
-#define DC_OFFSET  0  // DC offset in mic signal - if unusure, leave 0
-#define NOISE     100  // Noise/hum/interference in mic signal
-#define SAMPLES   60  // Length of buffer for dynamic level adjustment
-#define TOP       (N_PIXELS +1) // Allow dot to go slightly off scale
+#define N_PIXELS 30  // Number of pixels
+#define MIC_PIN 1
+#define LED_PIN 0
+#define SAMPLE_WINDOW 50
+#define THRESHOLD 500 // minimum level required to change colors
 
-byte
-  peak      = 0,      // Used for falling dot
-  dotCount  = 0,      // Frame counter for delaying dot-falling speed
-  volCount  = 0;      // Frame counter for storing past volume data
-  
-int
-  vol[SAMPLES],       // Collection of prior volume samples
-  lvl       = 10,     // Current "dampened" audio level
-  minLvlAvg = 0,      // For dynamic adjustment of graph low & high
-  maxLvlAvg = 512,
-  colorIndex = 0;
+unsigned int sample;
 
 Adafruit_NeoPixel  strip = Adafruit_NeoPixel(N_PIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
+unsigned int colorIndex = 0;
 uint32_t colors[] = {
   strip.Color(255, 0, 0),
   strip.Color(0, 255, 0),
@@ -52,33 +19,20 @@ uint32_t colors[] = {
 };
 
 // keeps track of how long it has been since the color changed
-unsigned long timeSinceChange = 0;
+unsigned long lastChange = 0;
 
 void setup() {
-  memset(vol, 0, sizeof(vol));
   strip.begin();
 }
 
 void loop() {
-  uint8_t  i;
-  uint16_t minLvl, maxLvl;
-  int      n, height;
-  n   = analogRead(MIC_PIN);                 // Raw reading from mic 
-  n   = abs(n - 512 - DC_OFFSET);            // Center on zero
-  n   = (n <= NOISE) ? 0 : (n - NOISE);      // Remove noise/hum
-  lvl = ((lvl * 7) + n) >> 3;    // "Dampened" reading (else looks twitchy)
   
-  // Calculate bar height based on dynamic min/max levels (fixed point):
-  height = TOP * (lvl - minLvlAvg) / (long)(maxLvlAvg - minLvlAvg);
-
-  if(height < 0L)       height = 0;      // Clip output
-  else if(height > TOP) height = TOP;
-  if(height > peak)     peak   = height; // Keep 'peak' dot at top
-
-  if (height > 15) {
+  unsigned int level = getLevel();
+  
+  if (level > 500) {
     // changes color if the signal is strong enough
     setStrip(colors[colorIndex]);
-    timeSinceChange = millis();
+    lastChange = millis();
   } 
   else {
     // go to the next color
@@ -89,37 +43,45 @@ void loop() {
     }
   }
 
-  if ((millis() - timeSinceChange) > (10000)) {
+  if (since(lastChange) > (10000)) {
     // turn off the lights if there hasn't been a sound for a while
     setStrip(strip.Color(0,0,0));
   }
 
-  strip.show(); // Update strip
-
-  vol[volCount] = n;                      // Save sample for dynamic leveling
-  if(++volCount >= SAMPLES) volCount = 0; // Advance/rollover sample counter
-
-  // Get volume range of prior frames
-  minLvl = maxLvl = vol[0];
-  for(i=1; i<SAMPLES; i++) {
-    if(vol[i] < minLvl)      minLvl = vol[i];
-    else if(vol[i] > maxLvl) maxLvl = vol[i];
-  }
-  
-  // minLvl and maxLvl indicate the volume range over prior frames, used
-  // for vertically scaling the output graph (so it looks interesting
-  // regardless of volume level).  If they're too close together though
-  // (e.g. at very low volume levels) the graph becomes super coarse
-  // and 'jumpy'...so keep some minimum distance between them (this
-  // also lets the graph go to zero when no sound is playing):
-  if((maxLvl - minLvl) < TOP) maxLvl = minLvl + TOP;
-  minLvlAvg = (minLvlAvg * 63 + minLvl) >> 6; // Dampen min/max levels
-  maxLvlAvg = (maxLvlAvg * 63 + maxLvl) >> 6; // (fake rolling average)
+  strip.show();
 }
 
+// returns a value between 0 and 1023 representing the sound level
+// from https://learn.adafruit.com/adafruit-microphone-amplifier-breakout/measuring-sound-levels
+unsigned int getLevel() {
+  unsigned long sampleStart = millis();  // Start of sample window
+  unsigned int peakToPeak = 0;   // peak-to-peak level
+ 
+  unsigned int signalMax = 0;
+  unsigned int signalMin = 1024;
+
+  while (since(sampleStart) < SAMPLE_WINDOW)
+  {
+    sample = analogRead(MIC_PIN);
+    if (sample < 1024)  // toss out spurious readings
+    {
+      signalMax = max(sample, signalMax);  // save just the max levels
+      signalMin = min(sample, signalMin);  // save just the min levels
+    }
+  }
+
+  return signalMax - signalMin;
+}
+
+// sets all the LEDs in the strip to the given color
 void setStrip(uint32_t color) {
   for(int i=0; i < N_PIXELS; i++) {  
     strip.setPixelColor(i, color);
   }
+}
+
+// returns the number of miliseconds since the given timestamp
+unsigned int since(unsigned int t) {
+  return millis() - t;
 }
 
--- readme.md
+++ readme.md
@@ -2,3 +2,6 @@ Arduino sketch for a sound-reactive LED strip.
 
 Based on:
 https://learn.adafruit.com/gemma-powered-neopixel-led-sound-reactive-drums?view=all#overview
+
+and:
+https://learn.adafruit.com/adafruit-microphone-amplifier-breakout/measuring-sound-levels
